105f3c2c26bb68b801421b92fa65b8d50625b556
Jacob MacDonald <jakemac@google.com>
2016-01-26 14:31:00 -0800

    don't run tests on js for now since they only work in pub-serve and we cant target that directly

--- test/deferred_library_test.dart
+++ test/deferred_library_test.dart
@@ -1,6 +1,10 @@
 // Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
+// TODO(jakemac): swap this to @TestOn('pub-serve') once
+// https://github.com/dart-lang/test/issues/388 is completed.
+@TestOn('!js')
 @initializeTracker
 library initialize.deferred_library_test;
 
--- test/initializer_cycle_error_test.dart
+++ test/initializer_cycle_error_test.dart
@@ -1,6 +1,10 @@
 // Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
+
+// TODO(jakemac): swap this to @TestOn('pub-serve') once
+// https://github.com/dart-lang/test/issues/388 is completed.
+@TestOn('!js')
 library initialize.initializer_cycle_error_test;
 
 import 'cycle_a.dart'; // Causes a cycle.
